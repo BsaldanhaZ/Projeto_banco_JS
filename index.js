66deb77a60be91df8a168a77ad505283c434fb26
Bruno Saldanha <bsaldanha1990@gmail.com>
2023-03-13 18:46:30 -0300

    criando o json

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ import {ContaCorrente} from "./ContaCorrente.js";
 import {Cliente} from "./Cliente.js";
 
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente(102, cliente2);
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
 
 console.log(contaCorrenteRicardo);
